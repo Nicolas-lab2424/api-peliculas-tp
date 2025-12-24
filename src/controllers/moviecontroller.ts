55ef2d6c62ed98533881d8e4421dd59e92e7dda4
Nicolas-lab2424 <Conigaleano4@gmail.com>
2025-12-23 23:57:24 -0300

    Add: filtros por query params en GET /movies

--- src/controllers/moviecontroller.ts
+++ src/controllers/moviecontroller.ts
@@ -1,12 +1,24 @@
+// src/controllers/movie.controller.ts
 import { Request, Response } from "express";
 import * as movieService from "../services/movieservice";
 
-export const getMovies = async (_req: Request, res: Response) => {
+export const getMovies = async (req: Request, res: Response) => {
   try {
-    const movies = await movieService.getAllMovies();
+    const { title, genre, ratingMin, ratingMax, year } = req.query;
+
+    const filters = {
+      title: title as string,
+      genre: genre as string,
+      ratingMin: ratingMin ? Number(ratingMin) : undefined,
+      ratingMax: ratingMax ? Number(ratingMax) : undefined,
+      year: year ? Number(year) : undefined,
+    };
+
+    const movies = await movieService.getAllMovies(filters);
     res.status(200).json(movies);
   } catch (error) {
     console.error("Error al obtener películas:", error);
     res.status(500).json({ message: "Error al obtener películas" });
   }
 };
+
--- src/services/movieservice.ts
+++ src/services/movieservice.ts
@@ -1,6 +1,37 @@
+
 import { Movie, IMovie } from "../models/moviemodel";
 
-export const getAllMovies = async (): Promise<IMovie[]> => {
-  const movies = await Movie.find();
+interface QueryFilters {
+  title?: string;
+  genre?: string;
+  ratingMin?: number;
+  ratingMax?: number;
+  year?: number;
+}
+
+export const getAllMovies = async (filters: QueryFilters): Promise<IMovie[]> => {
+  const query: any = {};
+
+  if (filters.title) {
+    query.title = { $regex: filters.title, $options: "i" };
+  }
+
+  if (filters.genre) {
+    query.genre = filters.genre;
+  }
+
+  if (filters.ratingMin !== undefined) {
+    query.rating = { ...query.rating, $gte: filters.ratingMin };
+  }
+
+  if (filters.ratingMax !== undefined) {
+    query.rating = { ...query.rating, $lte: filters.ratingMax };
+  }
+
+  if (filters.year !== undefined) {
+    query.releaseYear = filters.year;
+  }
+
+  const movies = await Movie.find(query);
   return movies;
 };
